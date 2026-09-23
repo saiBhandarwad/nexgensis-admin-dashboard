08c7d68d11e945b098dc3470cc45ec6896000965
saiBhandarwad <s333bhandarwad@gmail.com>
2026-09-23 11:57:42 +0530

    tailwind setup done

--- product-dashboard/src/App.jsx
+++ product-dashboard/src/App.jsx
@@ -1,9 +1,12 @@
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css'
 
+function App() {
+  
   return (
    <>
-   <h1>hello from sai</h1>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
    </>
   )
 }
